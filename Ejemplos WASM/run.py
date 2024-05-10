3bcb9c0c6f03da6704c81768ae61a986ae4ed724
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-05-09 21:14:48 -0600

    Add code to demo simple wasm functions

--- Ejemplos WASM/run.py
+++ Ejemplos WASM/run.py
@@ -13,7 +13,11 @@ def call_wasm_fun(file_name, fn_name, *args):
 
 def main():
     print(call_wasm_fun('simple.wat', 'meaning_of_life'))
-    print(call_wasm_fun('simple.wat', 'multiply', 2, 3))
+    print(call_wasm_fun('simple.wat', 'multiply', 3, 2))
+    print(call_wasm_fun('simple.wat', 'f', 5))
+    print(call_wasm_fun('simple.wat', 'f2c', 212.0))
+    print(call_wasm_fun('simple.wat', 'f2c', 32.0))
+    print(call_wasm_fun('simple.wat', 'f2c', -40.0))
 
 
 if __name__ == '__main__':
--- Ejemplos WASM/simple.wat
+++ Ejemplos WASM/simple.wat
@@ -19,4 +19,28 @@
     local.get $b
     i32.mul
   )
+
+  (func
+    (export "f")
+    (param $x i32)
+    (result i32)
+    i32.const 2
+    local.get $x
+    i32.mul
+    i32.const 7
+    i32.add
+  )
+
+  (func
+    (export "f2c")
+    (param $f f64)
+    (result f64)
+    local.get $f
+    f64.const 32.0
+    f64.sub
+    f64.const 5.0
+    f64.mul
+    f64.const 9.0
+    f64.div
+  )
 )
